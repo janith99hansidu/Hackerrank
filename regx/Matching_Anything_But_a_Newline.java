64605ac2ef252655b92e770fd1fa7a95b53a0436
Janith Hansidu Yapa <97206910+janith99hansidu@users.noreply.github.com>
2024-04-07 11:45:17 +0530

    create Matching_Anything_But_a_Newline.java

---
+++ regx/Matching_Anything_But_a_Newline.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Tester tester = new Tester();
+        tester.check("...\\....\\....\\...."); 
+    
+    }
+}
